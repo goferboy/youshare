fecb4796c4df490a4a3f7741c8288cae16dc78d2
Henry Baum <henryandrewbaum@gmail.com>
2020-12-04 21:19:46 -0500

    Getting somewhere with flask and socket io to interact with the front end

--- back-end/app.py
+++ back-end/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, g;
 from flask_cors import CORS;
 from playhouse.shortcuts import model_to_dict;
-from flask_socketio import SocketIO;
+from flask_socketio import SocketIO, send, emit, join_room;
 
 import models;
 from blueprints.sessions import session;
@@ -14,10 +14,13 @@ load_dotenv();
 import os;
 PORT = os.getenv("PORT");
 SECRET = os.getenv("SECRET");
+ROOMS = {};
 
 app = Flask(__name__);
+app.config['SECRET_KEY'] = SECRET;
+socketio = SocketIO(app, cors_allowed_origins="*");
 
-CORS(session, origins=['http://localhost:3000'], supports_credentials=True);
+CORS(app, origins=['http://localhost:3000'], supports_credentials=True);
 
 app.register_blueprint(session, url_prefix="/api/sessions");
 
@@ -31,6 +34,17 @@ def after_request(response):
     g.db.close();
     return response;
 
+@socketio.on('connection')
+def handle_my_custom_event(json):
+    print('received json: ' + str(json));
+    emit('connection', json);
+
+# @socketio.on('itsme')
+# def send_back():
+#     print("sending something back now");
+    
+
+
 if __name__ == '__main__':
     models.initialize();
-    app.run(debug=DEBUG, port=PORT);
+    socketio.run(app, debug=DEBUG, port=PORT);
--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
+import io from 'socket.io-client';
 import './App.css';
+
+const socket = io('http://localhost:8000');
+socket.on('connect', () => {
+  socket.emit('connection', {data: "HELLOOOOO"});
+  socket.on('connection', (res) => {
+    console.log(res);
+  })
+});
+
+try {
+  fetch('http://localhost:8000/api/sessions').then((res) => {
+    return res.json();
+  }).then((data) => {
+    console.log(data);
+  });
+}
+catch(err) {
+  console.log(err);
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <p>yes hello</p>
+        <p>Hello</p>
       </div>
     );
   }
